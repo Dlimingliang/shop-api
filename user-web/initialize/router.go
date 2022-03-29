4af0db4a84d22f32b383a0576c9175ae82064ada
limingliangaiduyangyang <lml2dyy@gmail.com>
2022-03-29 22:32:41 +0800

    跨域设置

--- user-web/initialize/router.go
+++ user-web/initialize/router.go
@@ -1,18 +1,28 @@
 package initialize
 
 import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+
 	"github.com/Dlimingliang/shop-api/user-web/api"
 	"github.com/Dlimingliang/shop-api/user-web/middlewares"
 	"github.com/Dlimingliang/shop-api/user-web/router"
-	"github.com/gin-gonic/gin"
 )
 
 func Routers() *gin.Engine {
 	ginRouter := gin.Default()
+	//设置跨域
+	ginRouter.Use(cors.New(cors.Config{
+		AllowAllOrigins:  true,
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"},
+		AllowHeaders:     []string{"*"},
+		AllowCredentials: true,
+		ExposeHeaders:    []string{"Content-Length", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type"},
+	}))
+	//设置路由
 	ginRouter.POST("login", api.PasswordLogin)
 	apiGroup := ginRouter.Group("v1")
-	apiGroup.Use(middlewares.JWTAuth())
-	apiGroup.Use(middlewares.IsAdmin())
+	apiGroup.Use(middlewares.JWTAuth()).Use(middlewares.IsAdmin())
 	router.InitUserRouter(apiGroup)
 	return ginRouter
 }
